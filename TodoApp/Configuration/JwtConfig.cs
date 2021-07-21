d1a189b65b6524e48a843f49785f683aa264672a
Adima Putra <adima.mahardika117@gmail.com>
2021-07-21 07:14:48 +0700

    Add project files.

---
+++ TodoApp/Configuration/JwtConfig.cs
@@ -0,0 +1,7 @@
+namespace RfToken.Configuration
+{
+    public class JwtConfig
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ TodoApp/Models/DTOs/Responses/RegistrationResponse.cs
@@ -0,0 +1,9 @@
+using RfToken.Configuration;
+
+namespace RfToken.Models.DTOs.Responses
+{
+    public class RegistrationResponse : AuthResult
+    {
+        
+    }
+}
